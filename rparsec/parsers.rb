075aefb25fcba227406d08f2cb4795768c97eb12
Koji Arai <jca02266@gmail.com>
2019-09-21 10:25:53 +0900

    Fix bugs: cannot call string_nocase() without include RParsec::Parsers

--- rparsec/parsers.rb
+++ rparsec/parsers.rb
@@ -371,19 +371,20 @@ class AreParser < Parser
   end
 end
 
-def downcase c
-  case when c.ord >= ?A.ord && c.ord <= ?Z.ord then (c.ord + (?a.ord - ?A.ord)).chr else c end
-end
-
 class StringCaseInsensitiveParser < Parser
   init :str, :msg
+  def _downcase c
+    case when c.ord >= ?A.ord && c.ord <= ?Z.ord then (c.ord + (?a.ord - ?A.ord)).chr else c end
+  end
+  private :_downcase
+
   def _parse ctxt
     if @str.length > ctxt.available
       return ctxt.expecting(@msg)
     end
     cur = 0
     for cur in (0...@str.length)
-      if downcase(@str[cur]) != downcase(ctxt.peek(cur))
+      if _downcase(@str[cur]) != _downcase(ctxt.peek(cur))
         return ctxt.expecting(@msg)
       end
     end
